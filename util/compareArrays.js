7a37792d7ec196ccebea5aed8f6f8728fa0515dc
Trisha Organ <patriciaorgan82@gmail.com>
2017-01-29 08:23:02 +0000

    refactor and reduce code

--- util/compareArrays.js
+++ util/compareArrays.js
@@ -28,13 +28,13 @@
  * @param prevArray is an array of objects
  * @param currArray is an array of objects
  * @return a string with HTML markup in it, should return null if error occurs.
- *    // Example, Given the following data set:
+ *  // Example, Given the following data set:
     //
-            // var prevArray = [ {_id:1, someKey: "RINGING", meta: { subKey1: 1234, subKey2: 52 } } ];
-            // var currArray = [ {_id:1, someKey: "HANGUP",  meta: { subKey1: 1234 } },
-            //     {_id:2, someKey: "RINGING", meta: { subKey1: 5678, subKey2: 207, subKey3: 52 } } ];
-    
-            // console.log( arrayDiffToHtmlTable( prevArray, currArray));
+    // var prevArray = [ {_id:1, someKey: "RINGING", meta: { subKey1: 1234, subKey2: 52 } } ];
+    // var currArray = [ {_id:1, someKey: "HANGUP",  meta: { subKey1: 1234 } },
+    //     {_id:2, someKey: "RINGING", meta: { subKey1: 5678, subKey2: 207, subKey3: 52 } } ];
+
+    // console.log( arrayDiffToHtmlTable( prevArray, currArray));
     //
     //  OUTPUT (Note this is a text representation... output should be an HTML table):
     //
@@ -50,142 +50,124 @@ var _ = require('lodash');
 
 module.exports.arrayDiffToHtmlTable = function( prevArray, currArray) {
     try {
-        var result = "";
+        
         if (typeof prevArray === 'undefined' || typeof currArray === 'undefined' ){
             throw("Parameter was undefined");
         }
+       
         // create table
-        result = '<table frame="box" cellspacing="10" align="center" width="90%">';
-            
-        var resultHeaderArray = []
-        // recursive call to reach each deep level and create headings for keys
-        prevArray.forEach(function(obj) {
-            extractKeys(obj, "", resultHeaderArray);
-        });
-        currArray.forEach(function(obj) {
-            extractKeys(obj, "", resultHeaderArray);
-        });
-        
-        // create all possible combination headers in HTML string
+        var result = '<table frame="box" cellspacing="10" align="center" width="90%">';
+
+        // create headers
+        var resultHeaderArray = createHeaders(prevArray, currArray);
         resultHeaderArray.forEach(function(value){
-            result = result + "<th>" + value + "</th>";
-        })
-        
-        /**
-         * Assumption: if a whole row is deleted in the currArray compared to prevArray, I will not show it at all in the final table
-         * Alternative implemenations could be - show the row with *delete* in each column,
-         * Or show teh row with the _id filled in and all other appropriate columns with *delete*
-         */
-        //create rows          
-        //use the currArray to determin the number of rows to add
-        currArray.forEach( function (currRow) {
-            
-            //get the object with the correct _id from prevArray
-            var prevRow = getObjectFromArray(prevArray, currRow._id.toString());
-            if (prevRow) {
-                result += '<tr>';
-                // traverse through the column headers so each cell is added in the right location
-                resultHeaderArray.forEach( function(header) {
-                    var currValue = currRow[header];
-                    var prevValue = prevRow[header];
-                    if (header.indexOf('_') > 0) { 
-                        var path = header.split("_").join(".");
-                        var currValue = _.get(currRow, path);
-                        var prevValue = _.get(prevRow, path);
-                    }
-                
-                    if (prevValue) {
-                        if ( prevValue === currValue) {
-                            result += addCell(currValue, false);
-                        } else if (!currValue){
-                            result += addCell('Deleted', true);
-                        } else {
-                            result += addCell(currValue, true);
-                        }
-                    } else if (!currValue) {
-                        result += addCell('', false);
-                    } else {
-                        result += addCell(currValue, true);
-                    }
-                });
-            } else {
-                resultHeaderArray.forEach( function(header) {
-                    // Assumption: no other key except _id starts with an underscore
-                    var currValue = currRow[header];
-                    if (header.indexOf('_') > 0) { 
-                        var path = header.split("_").join(".");
-                        currValue= _.get(currRow, path);
-                    }
-                    if (!currValue){
-                        result += addCell("", false);
-                    } else {
-                        result += addCell(currValue,true);
-                    }
-                });  
-            }
-            result += '</tr>';  
+            result += "<th>" + value + "</th>";
         });
-                       
+        
+        // create rows 
+        result += createRows(prevArray, currArray, resultHeaderArray);
+        
+        result += "</table>";
+        return result;     
+        
     } catch (err) {
         console.log("Error occured in arrayDiffToHtmlTable %O", err);
         return null;
     }
-    // close off the Table tag 
-    result += "</table>";
-    return result;
 }
 
 /**
- * Extract the Keys from all levels of an object
- * @param {Object} Assumption: that the Object will not contain Arrays inside it, just key values pair and other objects.
- * @param {String} hold the key for a deeper object
- * @param {Array} passing by reference the Array to add all keys
- * @return {undefined} only if no object was passed, otherwise updating array passed by reference
+ * Create headers, Extract the Keys from all levels of an object from the concatinated arrays
+ * Assumption: that the Object will not contain Arrays inside it, just key values pair and other objects.
+ * @param {Array} previous Array
+ * @param {Array} current Array
+ * @return {Array} all possible headings
  * 
  */
-function extractKeys(obj, key, resultHeaderArray){
-    if (!obj) {
-        return;
-    }
-    var keysArray= Object.keys(obj);
-    
-    for (var i = 0; i < keysArray.length; i++) {
-        var newKey = key ? key + '_' + keysArray[i] : keysArray[i];
-        if (resultHeaderArray.indexOf(newKey) > -1){
-            continue;
-        } else if (typeof obj[keysArray[i]] == 'object'){
-            extractKeys(obj[keysArray[i]], newKey, resultHeaderArray);
-       } else {
-            resultHeaderArray.push(newKey); 
-       }
-    }
-}
+function createHeaders(prevArray, currArray) {
+    var headers = [];
 
-/**
- * Get Object From Array with correct id
- * @param {Array} arr, array of objects
- * @param {String} id, string version of id for type comparison match
- * @return the whole object where id is found, otherwise null
- *
- */
-function getObjectFromArray(arr, id) {
-    var result = null;
-    arr.forEach(function(item){
-        if (item['_id'].toString() === id) {
-            result = item;
+    function extractKeys(obj, key){
+        if (!obj) {
+            return;
         }
+        var keysArray= Object.keys(obj);
+        
+        for (var i = 0; i < keysArray.length; i++) {
+            var newKey = key ? key + '_' + keysArray[i] : keysArray[i];
+            if (headers && headers.indexOf(newKey) > -1){
+                continue;
+            } else if (typeof obj[keysArray[i]] == 'object'){
+                extractKeys(obj[keysArray[i]], newKey);
+            } else {
+                headers.push(newKey); 
+            }
+        }
+    }
+    // recursive call to reach each deep level and create headings for keys
+    prevArray.concat(currArray).forEach(function(obj) {
+        extractKeys(obj, "");
     });
-    return result;
+    
+    return headers;
 }
 
 /**
- * Create the HTML string for table cells, based on bold and value
+ * Create HTML rows based on if the cells have changed or not
+ * Assumption:  if a whole row is deleted in the currArray compared to prevArray, I will not show it at all in the final table
+ *              Alternative implemenations could be - show the row with *delete* in each column,
+ *              Or show the row with the _id filled in and all other appropriate columns with *delete*
+ * @param {Array} previous Array
+ * @param {Array} current Array
+ * @param {Array} headings Array
+ * @return {String} HTML string of table rows
+ */
+function createRows(prevArray, currArray, headings){
+    var rows = '';    
+    //use the currArray to determin the number of rows to add
+    currArray.forEach( function (currRow) {
+            rows += '<tr>';
+            // traverse through the column headers so each cell is added in the right location
+            headings.forEach( function (header) {
+                //get the object with the correct _id from prevArray
+                var prevRow = _.find(prevArray, {_id:currRow._id});
+                var currValue = currRow[header];
+                var prevValue = prevRow ? prevRow[header] : undefined;
+                
+                //Assumption only _id key starts with an underscore
+                if ( header.indexOf('_') > 0 ) { 
+                    var path = header.split("_").join(".");
+                    currValue = _.get(currRow, path);
+                    prevValue = _.get(prevRow, path);
+                }
+            
+                if (prevValue) {
+                    if ( prevValue === currValue ) {
+                        rows += addCell(currValue, false);
+                    } else if (!currValue){
+                        rows += addCell('Deleted', true);
+                    } else {
+                        rows += addCell(currValue, true);
+                    }
+                } else if (!currValue) {
+                    rows += addCell('', false);
+                } else {
+                    rows += addCell(currValue, true);
+                }
+            });
+        rows += '</tr>';  
+    });
+    return rows;
+}
+
+/**
+ * Create the HTML string for table cells, based on bold/highlighted and value
  * @param {String} cell value
  * @param {Boolean} true of style is to be added, and false otherwise
  * @return {String} HTML tagged td
  * 
  */
-function addCell(value,bold) {
+function addCell(value, bold) {
     var style = bold ? 'bgcolor="#00FF00"' : '';
     return '<td align="center"' + style +  '>' + value + '</td>';
     
